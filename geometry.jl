61a8071289c29275ab27227384ee5a2ec3f0659b
HiroIshida <spitfire.docomo@gmail.com>
2018-10-22 20:08:52 +0900

    add rectangle

--- geometry.jl
+++ geometry.jl
@@ -1,15 +1,40 @@
+using StaticArrays
+using StaticArrays.ImmutableArrays
+using PyPlot
 export Polygon, isInside, isIntersect
 
-struct Polygon
+const Vec2f = SVector{2, Float64}
+const Vec4f = SVector{4, Float64}
+
+abstract type Polygonic end
+
+struct Polygon <: Polygonic
     N::Int 
-    V::Vector{Vector2}
-    function Polygon(V) #V nust be counterclockwise
-        N = length(V)
+    V::Vector{Vec2f}
+    function Polygon(V_::AbstractVector) #V nust be counterclockwise
+        N = length(V_)
+        V = Vec2f[]
+        for v in V_
+            push!(V, Vec2f(v))
+        end
         new(N, V)
     end
 end
 
-@inline function isInside(this::Polygon, q::Vector2)
+struct Rectangle <: Polygonic
+    N::Int
+    V::Vector{Vec2f}
+    function Rectangle(center::T, width::Float64, height::Float64) where {T<:AbstractVector}
+        V1 = Vec2f(center + [-width -height]*0.5)
+        V2 = Vec2f(center + [+width -height]*0.5)
+        V3 = Vec2f(center + [+width +height]*0.5)
+        V4 = Vec2f(center + [-width +height]*0.5)
+        new(4, [V1, V2, V3, V4])
+    end
+end
+
+
+@inline function isInside(this::T, q::Vec2f) where {T<:Polygonic}
     # note: n'x = n'b
     q_vec = [q[1]; q[2]]
     for n = 1:this.N
@@ -23,7 +48,14 @@ end
     return true
 end
 
-@inline function isIntersect(this::Polygon, q1::Vector2, q2::Vector2)::Bool
+
+@inline function isIntersect(this::T, q1_::AbstractVector, q2_::AbstractVector) where {T<:Polygonic}
+    @views q1 = Vec2f(q1_[1:2])
+    @views q2 = Vec2f(q2_[1:2])
+    return isIntersect(this, q1, q2)
+end
+
+@inline function isIntersect(this::T, q1::Vec2f, q2::Vec2f)::Bool where {T<:Polygonic}
     for n = 1:this.N
         p1 = this.V[n]
         if n<this.N
@@ -35,7 +67,8 @@ end
     end
     return false
 end
-function show(this::Polygon, color=:red)
+
+function show(this::T, color=:red) where {T<:Polygonic}
     for n = 1:this.N
         if n<this.N
             x = [this.V[n][1], this.V[n+1][1]]
@@ -49,7 +82,7 @@ function show(this::Polygon, color=:red)
 end
 
 
-@inline function isIntersect(p1::Vector2, p2::Vector2, q1::Vector2, q2::Vector2)::Bool
+@inline function isIntersect(p1::Vec2f, p2::Vec2f, q1::Vec2f, q2::Vec2f)::Bool
     # solve [p2-p1, q2-q1]*[s; t]=[q1-p1] or A*[s; t]=B
     # for computational efficiency, not using inv() func
     # A = [a, b; c, d]
@@ -69,12 +102,15 @@ end
 end
 
 function test()
-    v1 = (0.0, 0.0)
-    v2 = (1.0, 0.0)
-    v3 = (1.0, 1.0)
-    v4 = (0.0, 1.0)
+    """
+    v1 = [0.0, 0.0]
+    v2 = [1.0, 0.0]
+    v3 = [1.0, 1.0]
+    v4 = [0.0, 1.0]
     V = [v1, v2, v3, v4]
     P = Polygon(V)
-    isInside(P, (1.2, 0.8))
+    """
+    P = Rectangle([0.5 0.5], 1.0, 1.0)
+    isInside(P, Vec2f([1.2, 0.8]))
 end
 
